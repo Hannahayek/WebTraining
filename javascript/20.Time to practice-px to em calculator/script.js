216752515c4b3d3d0f91466b1674941cac96a157
hayek <hayek@HANNA-PC>
2018-10-21 21:33:05 +0800

    110. Solution: pixel to em converter

--- javascript/20.Time to practice-px to em calculator/script.js
+++ javascript/20.Time to practice-px to em calculator/script.js
@@ -1,1 +1,19 @@
 // em = required / parent 
+var Calc =() => {
+    var requiredVal=document.getElementById('required').value;
+    var parentVal=document.getElementById('parent').value;
+    var result=document.getElementById('result');
+    
+    
+    if(!requiredVal | !parentVal){
+        alert("Please fill both numbers");
+    }
+    else
+    {
+        result.innerHTML=requiredVal / parentVal;
+    }
+    
+}
+
+var btn=document.querySelector('button');
+btn.addEventListener('click',Calc);
