5c6e7b59b7d693d0172376d72a1500f28f9ab9ed
hayek <hayek@HANNA-PC>
2018-10-28 21:37:24 +0800

    125. Const and let

---
+++ javascript/31.Shape matcher game/script.js
@@ -0,0 +1,23 @@
+
+//let is vairable for blocks, var is globale
+let currentValue=0;
+let playing=false;
+let shape1;
+let shape2;
+
+const shapes=[
+{color:'#FF59FE',width:'250',height:'160'},
+{color:'#FF59FE',width:'270',height:'150'},
+{color:'#FFCA3A',width:'320',height:'170'},
+{color:'#FFCA3A',width:'310',height:'180'},
+{color:'#8AC926',width:'190',height:'160'},
+{color:'#8AC926',width:'200',height:'175'},
+{color:'#1982C4',width:'380',height:'185'},
+{color:'#1982C4',width:'400',height:'120'},
+{color:'#6A4C93',width:'370',height:'145'},
+{color:'#6A4C93',width:'440',height:'160'},
+
+]
+console.log(shapes[2]);
+
+
