471432d70aebf3ffe67280fa0cb383030809f82d
Hannahayek <42571381+Hannahayek@users.noreply.github.com>
2018-10-31 16:18:59 +0800

    136. Map through results

--- song-finder/script.js
+++ song-finder/script.js
@@ -3,7 +3,35 @@ fetch(url)
 .then((response)=>response.json()) //we pass the data to Json. then to data
 .then((data)=>{
 
-    console.log(data);
+   const artists=data.results;      //results from the api
+return artists.map(result =>{
+   const songCotainer=document.getElementById('songs'),
+          article=document.createElement('article'),  //all are constants we can seperate by ,
+          artist=document.createElement('p'),
+          song=document.createElement('p'),
+          img=document.createElement('img'),
+          audio=document.createElement('audio'),
+          audioSource=document.createElement('source')
+         
+
+          artist.innerHTML=result.artistName      ///artistName and other names can be found in result
+          song.innerHTML=result.trackName
+          img.src=result.artworkUrl100
+          audioSource.src=result.previewUrl
+          audio.setAttribute('controls','')
+
+          // we set childs for article
+          article.appendChild(artist)
+          article.appendChild(song)
+          article.appendChild(img)
+          article.appendChild(audio)
+          //append child audio source for audio
+          audio.appendChild(audioSource)
+          //append article to the main
+          songCotainer.appendChild(article)
+
+})
+
 })
 .catch(error=> console.log("request failed",error))
 
