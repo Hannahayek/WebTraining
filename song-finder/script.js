9760b443a4297a2121f0e3e018f43e0e6a96ccf8
Hannahayek <42571381+Hannahayek@users.noreply.github.com>
2018-10-31 17:49:56 +0800

    137. Creating our search query

--- song-finder/script.js
+++ song-finder/script.js
@@ -1,38 +1,59 @@
-const url="https://itunes.apple.com/search?term=Beyonce";
-fetch(url)
-.then((response)=>response.json()) //we pass the data to Json. then to data
-.then((data)=>{
+let term='';
+const updateTerm=()=>{
+    term=document.getElementById('searchInput').value;
 
-   const artists=data.results;      //results from the api
-return artists.map(result =>{
-   const songCotainer=document.getElementById('songs'),
-          article=document.createElement('article'),  //all are constants we can seperate by ,
-          artist=document.createElement('p'),
-          song=document.createElement('p'),
-          img=document.createElement('img'),
-          audio=document.createElement('audio'),
-          audioSource=document.createElement('source')
-         
+    if(!term ||term===''){
 
-          artist.innerHTML=result.artistName      ///artistName and other names can be found in result
-          song.innerHTML=result.trackName
-          img.src=result.artworkUrl100
-          audioSource.src=result.previewUrl
-          audio.setAttribute('controls','')
+        alert("please input search term")
+    }
+    else{
 
-          // we set childs for article
-          article.appendChild(artist)
-          article.appendChild(song)
-          article.appendChild(img)
-          article.appendChild(audio)
-          //append child audio source for audio
-          audio.appendChild(audioSource)
-          //append article to the main
-          songCotainer.appendChild(article)
+           
 
-})
+        const url="https://itunes.apple.com/search?term="+term;
+        fetch(url)
+        .then((response)=>response.json()) //we pass the data to Json. then to data
+        .then((data)=>{
+        
+           const artists=data.results;      //results from the api
+        return artists.map(result =>{
+           const songCotainer=document.getElementById('songs'),
+                  article=document.createElement('article'),  //all are constants we can seperate by ,
+                  artist=document.createElement('p'),
+                  song=document.createElement('p'),
+                  img=document.createElement('img'),
+                  audio=document.createElement('audio'),
+                  audioSource=document.createElement('source')
+                 
+        
+                  artist.innerHTML=result.artistName      ///artistName and other names can be found in result
+                  song.innerHTML=result.trackName
+                  img.src=result.artworkUrl100
+                  audioSource.src=result.previewUrl
+                  audio.setAttribute('controls','')
+        
+                  // we set childs for article
+                  article.appendChild(artist)
+                  article.appendChild(song)
+                  article.appendChild(img)
+                  article.appendChild(audio)
+                  //append child audio source for audio
+                  audio.appendChild(audioSource)
+                  //append article to the main
+                  songCotainer.appendChild(article)
+        
+        })
+        
+        })
+        .catch(error=> console.log("request failed",error))
 
-})
-.catch(error=> console.log("request failed",error))
+    }
+}
+
+
+
+
+const searchButtom=document.querySelector('button')
+searchButtom.addEventListener('click',updateTerm)
 
 
