a649d0c4e146f7a11bf9b26a61644ab10323b319
Hannahayek <42571381+Hannahayek@users.noreply.github.com>
2018-11-06 15:53:25 +0800

    159. Mongoose models

---
+++ Lets-Travel/models/hotels.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const hotelSchema=new moongose.Schema({
+hote_name: {
+type:String,
+required:'Hotel Name is required',
+max:32,
+trim:true
+},
+hotel_description:{
+    type:String,
+    required:"Hotel description is required",
+    trim:true
+},
+image:String,
+
+star_rating:{
+type:Number,
+required:"Hotel Ratings is required",
+max:5,
+},
+country:{
+    type:String,
+    required:true,
+    trim:true
+},
+
+cost_per_night:{
+    type:Number,
+    required:'Cost per night is required'
+},
+available:{
+    type:Boolean,
+    required:'Availablety is required'
+}
+
+});
+
+
+
+//export model
+
+module.exports=mongoose.model('Hotel',hotelSchema);
