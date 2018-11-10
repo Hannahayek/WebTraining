5a289879fcb5738f20079c845834367d53f0d71a
hayek <hayek@HANNA-PC>
2018-11-10 22:31:37 +0800

    162. Querying the database

--- Lets-Travel/controllers/hotelController.js
+++ Lets-Travel/controllers/hotelController.js
@@ -4,8 +4,14 @@ exports.homePage =(req,res) =>{''
 
 };
 
-exports.listAllHotels=(req,res)=>{
-    res.render('all_hotels', { title: 'All Hotels' });
+exports.listAllHotels= async (req,res,next)=>{
+    try {
+        const allHotels=await Hotel.find({available: {$eq: true}});
+         res.render('all_hotels', { title: 'All Hotels',allHotels });
+        // res.json(allHotels);
+    } catch (error) {
+     next(error)   
+    }
 
 };
 
